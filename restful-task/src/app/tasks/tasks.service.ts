dafe2964f40ba7272e92e02316eb18358c9006bf
JoseRodriguez-CR <josemariorodriguezc@gmail.com>
2021-12-06 13:58:48 -0600

    Updating Files

--- restful-task/src/app/tasks/tasks.service.ts
+++ restful-task/src/app/tasks/tasks.service.ts
@@ -9,11 +9,18 @@ export class TasksService {
 
   constructor( private _http: HttpClient ) {
     this.getAllTask();
+    this.showTask();
   }
 
   getAllTask() {
     let observable = this._http.get( 'http://localhost:8080/api' );
     observable
     .subscribe(data=> console.log("Now you can see our Tasks API", data));    
+  }
+
+  showTask() {
+    let observable = this._http.get( 'http://localhost:8080/api/study2' );
+    observable
+    .subscribe(data=> console.log("Now you can see our Study Task API", data));    
   }
 }
